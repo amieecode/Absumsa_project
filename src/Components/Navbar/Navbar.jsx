78219d25afcaa4b508b6e5fba8e8d67c138fedf3
enwelum chiamaka <enwelumchiamaka@gmail.com>
2024-04-03 13:25:04 +0100

    First Commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='fixed top-0 right-0 w-full bg-white text-black shadow-md'>
+      <div className="container">
+        <div className='flex'>
+    
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Navbar;
